6aee27109fb6c6df3addae6496c18aaea3b17052
A Student <student@nowhere.edu>
2025-10-26 01:21:44 +0000

    Oct 25, 2025, 6:21 PM

--- main.cpp
+++ main.cpp
@@ -2,12 +2,4 @@
 #include <iomanip>
 using namespace std;
 
-double getShippingCost(int shipMethod, double cost);
-int main() {
-double cost;
-int shipMethod;
-cout << "Enter the cost of your order: ";
-cin >> cost;
-cout << "Enter your shipping method: ";
-cin >> shipMethod;
-double shipping = getShippingCost(shipMethod, cost);
+
