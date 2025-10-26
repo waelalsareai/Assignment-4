db34e0974345904163eee493e51101b871552d56
A Student <student@nowhere.edu>
2025-10-26 01:09:45 +0000

    Oct 25, 2025, 6:09 PM

--- tests.cpp
+++ tests.cpp
@@ -52,10 +52,10 @@ else if (shipMethod == 2) {
     return 0.0;  // Free shipping for orders over $300
 }
 else if (shipMethod == 3) {
-    if (cost <= 50)
+    if (cost <= 35)
         return 5.0;  // $5 flat rate for orders ≤ $50
     else if (cost <= 100)
-        return 10.0;  // $10 flat rate for orders between $51 and $100
+        return 8.0;  // $10 flat rate for orders between $51 and $100
     else
         return 0.0;  // Free shipping for orders over $100
 }
