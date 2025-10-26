330799c5196e1452a2fb96d3d936e4605802a49f
A Student <student@nowhere.edu>
2025-10-26 01:26:38 +0000

    Oct 25, 2025, 6:26 PM

--- main.cpp
+++ main.cpp
@@ -4,4 +4,10 @@ using namespace std;
 
 double getShippingCost(int shipMethod, double cost) {
     if (cost < 0 || shipMethod < 1 || shipMethod > 3) return -1;
+    // 1 = Drone
+    if (shipMethod == 1) {
+        if (cost < 100) return -1;        // Not available
+        if (cost < 500) return 50.0;      // $50 for $100–$499.99
+        return cost * 0.10;               // 10% for $500+
+    }
     
