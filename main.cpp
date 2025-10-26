bdf737e60b464c71312e7456103c74647674c74f
A Student <student@nowhere.edu>
2025-10-26 01:31:59 +0000

    Oct 25, 2025, 6:31 PM

--- main.cpp
+++ main.cpp
@@ -27,3 +27,8 @@ double getShippingCost(int shipMethod, double cost) {
 int main() {
     double cost;
     int shipMethod;
+    cout << "Enter the cost of your order: ";
+    cin >> cost;
+    cout << "Enter your shipping method: ";
+    cin >> shipMethod;
+    double shipping = getShippingCost(shipMethod, Cost);
